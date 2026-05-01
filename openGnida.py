60360130add6aa63b1f4d065c89fd3b14a2d8221
Даниил <daniilaev415@gmail.com>
2026-05-01 18:01:03 +0300

    Функция филлер и тестовый py файл

--- openGnida.py
+++ openGnida.py
@@ -1,8 +1,21 @@
 def main():
     pass
 
-def volumeFiller(fileSize: int) -> Exception:
-    pass
+def volume_filler(file_size: int) -> Exception:
+    number = 1
+    current_exception = None
+    for attempt in range(5):
+        try:
+            while True:
+                with open(f"dumb_file{file_size}_{number}.bin", "wb") as file:
+                    file.write(b'\xFF'*file_size)
+                    file.close()
+                number += 1
+        except Exception as exp:
+            current_exception = exp
+    return current_exception
+
+
 
 
 confirm = False

